4c24505112781458a59af2e9d0d736e7b891eec3
Regina laurentino <reginalaurentino54@gmail.com>
2021-07-22 19:55:07 -0300

    contact

--- Negocio/EmailService.cs
+++ Negocio/EmailService.cs
@@ -14,7 +14,7 @@ namespace Negocio
 
         public EmailService() {
             server = new SmtpClient();
-            server.Credentials = new NetworkCredential("ponermail", "ponercontrasenia");
+            server.Credentials = new NetworkCredential("pruebaparaprog3@gmail.com", "programacion389");
             server.EnableSsl = true;
             server.Port = 587;
             server.Host = "smtp.gmail.com";
@@ -22,11 +22,21 @@ namespace Negocio
 
         public void armarcorreo(string emaildestino, string asunto, string cuerpo) {
             email = new MailMessage();
-            email.From = new MailAddress("noresponder@lalalalala");
+            email.From = new MailAddress("noresponder@pruebaparaprog3gmail.com");
             email.To.Add(emaildestino);
             email.Subject = asunto;
             email.IsBodyHtml = true;
             email.Body = "<h1>Gracias por tu compra </h1>";
+        }
+
+        public void armarcorreo(string emaildestino, string asunto, string cuerpo, string Nombre)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("noresponder@pruebaparaprog3gmail.com");
+            email.To.Add(emaildestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            email.Body = "<h1>Mi nombre es  </h1>"+Nombre;
         }
 
         public void enviarmail(){
